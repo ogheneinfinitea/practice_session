3e36baea00821ea70ab98172c04f83c147987ec5
Eduotu Ogheneriye Samson <ogheneinfinitea@gmail.com>
2019-08-07 17:01:12 +0100

    created new file for practice session

---
+++ string.py
@@ -0,0 +1,37 @@
+# str = "Hello World!"
+
+# print str          # Prints complete string
+# print str[0]       # Prints first character of the string
+# print str[2:5]     # Prints characters starting from 3rd to 5th
+# print str[2:]      # Prints string starting from 3rd character
+# print str * 2      # Prints string two times
+# print str + "TEST" # Prints concatenated string
+
+# list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+# tinylist = [123, 'john']
+
+# print (list )         # Prints complete list
+# print (list[0])       # Prints first element of the list
+# print (list[1:3])     # Prints elements starting from 2nd till 3rd 
+# print (list[2:] )     # Prints elements starting from 3rd element
+# print (tinylist * 2)  # Prints list two times
+# print (list + tinylist) # Prints concatenated lists
+
+
+# del list
+# print (list)
+
+#program to test if statment
+# var = 100
+# if ( var == 100 ) : 
+#     print ("Value of expression is 100")
+# print ("Good bye!")
+
+# program to check if a number is thesame
+# var =50
+# if (var==100):
+#     print ("The numbers are the same")
+# elif (var == 50):
+#     print ("number is 50")
+
+
